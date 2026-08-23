7d7ffd6e8f95774fdff278b91b7e0e4b0ee8c00e
Theo Rozier <contact@theorozier.fr>
2026-08-23 23:01:39 +0200

    Removed wot-cli

--- wot-cli/src/common/entity.rs
+++
@@ -1,7 +0,0 @@
-//! Base module for defining all entity-relating codecs.
-
-pub mod login;
-pub mod account;
-
-pub use login::Login;
-pub use account::Account;
--- wot-cli/src/common/mod.rs
+++
@@ -1,7 +0,0 @@
-//! This module contains common code for base and client applications.
-
-pub mod server_settings;
-pub mod entity;
-
-
-pub use server_settings::ServerSettings;
